7d72502857c1d0a63b68363ffab790e9debe49b7
User <user@Users-MacBook-Pro.local>
2019-11-29 22:24:58 +0000

    animal show route added,tested,needs fixed

--- controllers/animal_controller.rb
+++ controllers/animal_controller.rb
@@ -6,5 +6,12 @@ also_reload('../models/*')
 #Index set up
 get '/animals' do
   @animals = Animal.all()
-  erb ( :"animals/index" )
+  erb (:"animals/index")
+end
+
+#show route
+get '/animals/:id' do
+  id = params[:id].to_i()
+  @animals = Animal.find(id)
+  erb (:"animals/show")
 end
