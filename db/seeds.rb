b3f54f0aec4e8867b7d11a56f7f120f5373e006d
User <user@Users-MacBook-Pro.local>
2019-11-29 14:41:35 +0000

    added more owners,animals,adoptions to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,34 @@ require("pry")
 
   animal1.save()
 
+  animal2 = Animal.new({
+    "name" => "Thor",
+    "adopt_status" => "training/vet care",
+    "type" => "Cat",
+    "breed" => "Persion Mix",
+    "admis_date" => "9/10/2019",
+    "good_with_kids" => "No",
+    "good_with_other_pets" => "Yes",
+    "need_attention" => "No"
+    })
+
+  animal2.save()
+
+  animal3 = Animal.new({
+    "name" => "Neo",
+    "adopt_status" => "Adoptable",
+    "type" => "Cat",
+    "breed" => "Domestic Short-Hair",
+    "admis_date" => "17/11/2019",
+    "good_with_kids" => "Yes",
+    "good_with_other_pets" => "Yes",
+    "need_attention" => "No"
+    })
+
+  animal3.save()
+
+
+
   owner1 = Owner.new({
     "name" => "Jeff Lebowski",
     "address" => "23 Mable Road, MW1 5XL",
@@ -29,13 +57,53 @@ require("pry")
 
     owner1.save()
 
-  adoption1 = Adoption.new({
+  owner2 = Owner.new({
+    "name" => "Postman Pat",
+    "address" => "2 Cleo Lane, FK1 5PS",
+    "suitablity" => "Suitable",
+    "has_kids" => "No",
+    "has_other_pets" => "Yes",
+    "give_attention" => "No"
+
+    })
+
+    owner2.save()
+
+  owner3 = Owner.new({
+    "name" => "Helen Fafferty",
+    "address" => "33 Jaffa Road, G1 2TL",
+    "suitablity" => "Not Suitable",
+    "has_kids" => "Yes",
+    "has_other_pets" => "No",
+    "give_attention" => "No"
+
+    })
+
+    owner3.save()
+
+    adoption1 = Adoption.new({
     "animal_id" => animal1.id,
     "owner_id" => owner1.id,
     "adoption_date" => "30/11/2019"
     })
 
   adoption1.save()
+
+  adoption2 = Adoption.new({
+    "animal_id" => animal2.id,
+    "owner_id" => owner2.id,
+    "adoption_date" => "1/12/2019"
+    })
+
+  adoption2.save()
+
+  adoption3 = Adoption.new({
+    "animal_id" => animal3.id,
+    "owner_id" => owner1.id,
+    "adoption_date" => "26/11/2019"
+    })
+
+  adoption3.save()
 
   binding.pry
 
