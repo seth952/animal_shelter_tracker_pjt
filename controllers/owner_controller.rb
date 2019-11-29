3703451e0b0402c0f3717b92f211d6f0feea4b08
User <user@Users-MacBook-Pro.local>
2019-11-29 21:00:12 +0000

    owner index set up,tested,all good

--- controllers/owner_controller.rb
+++ controllers/owner_controller.rb
@@ -3,7 +3,7 @@ require('sinatra/contrib/all')
 require_relative('../models/owner.rb')
 also_reload('../models/*')
 
-#Index
+#Index set up
 get '/owners' do
   @owners = Owner.all()
   erb ( :"owners/index" )
